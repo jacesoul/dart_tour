211a23264a08cd587cac3d8e9d518559585925d4
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2023-01-15 22:22:14 +0900

    Lists

--- main.dart
+++ main.dart
@@ -1,8 +1,11 @@
 void main() {
-  String name = "jace";
-  bool alive = true;
-  int age = 30;
-  double money = 88.99;
-  num x = 12;
-  x = 1.1;
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+  print(numbers);
 }
