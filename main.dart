bcafd1251c651eb6083079eb44c229a9fc886a4a
jaceseol <seolju90@gmail.com>
2023-01-25 20:26:24 +0900

    Sets

--- main.dart
+++ main.dart
@@ -1,13 +1,8 @@
 void main() {
-  Map<List<int>, bool> player = {
-    [1, 2, 3, 4, 5]: true,
-    [6, 7, 8, 9, 10]: false,
-  };
-
-  List<Map<String, Object>> players = [
-    {'name': 'jace', 'xp': 100.99},
-    {'name': 'jace1', 'xp': 100.99},
-    {'name': 'jace2', 'xp': 100.99},
-    {'name': 'jace3', 'xp': 100.99}
-  ];
+  Set<int> numbers = {1, 2, 3, 4};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
 }
