7e10f685f54d3544a1c5be73c18f6ff765e3bcb8
jaceseol <seolju90@gmail.com>
2023-02-07 19:29:28 +0900

    Named Constructors Practice

--- main.dart
+++ main.dart
@@ -1,28 +1,12 @@
 class Player {
   final String name;
-  int xp, age;
+  int xp;
   String team;
 
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age,
-  });
-
-//named parameter
-  Player.createBluePlayer({required String name, required int age})
-      : this.age = age,
-        this.name = name,
-        this.team = "blue",
-        this.xp = 0;
-
-// positional parameter
-  Player.createRedPlayer(String name, int age)
-      : this.age = age,
-        this.name = name,
-        this.team = "red",
-        this.xp = 0; 
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
 
   void sayHello() {
     print("Hi my name is $name");
@@ -30,13 +14,26 @@ class Player {
 }
 
 void main() {
-  var bluePlayer = Player.createBluePlayer(
-    name: "jace",
-    age: 24,
-  );
+  var apiData = [
+    {
+      "name": "jace",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "mark",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lisa",
+      "team": "red",
+      "xp": 0,
+    }
+  ];
 
-  var redPlayer = Player.createRedPlayer(
-     "jace",
-     24,
-  );
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
